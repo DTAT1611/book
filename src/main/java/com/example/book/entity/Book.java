0c61ab92d72d70605556b9f0b760535926fc0c53
DTAT1611 <113090251+DTAT1611@users.noreply.github.com>
2023-05-25 11:03:18 +0700

    first commit

---
+++ src/main/java/com/example/book/entity/Book.java
@@ -0,0 +1,29 @@
+package com.example.book.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "author")
+    private String author;
+    @Column(name = "price")
+    private Double price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
